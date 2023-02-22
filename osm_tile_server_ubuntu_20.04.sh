2db49ea937712488f4ded6e0178a9737c1a3ebf0
dbelkovsky <37294176+dbelkovsky@users.noreply.github.com>
2023-02-22 03:54:48 +0300

    Update osm_tile_server_ubuntu_20.04.sh

--- osm_tile_server_ubuntu_20.04.sh
+++ osm_tile_server_ubuntu_20.04.sh
@@ -47,7 +47,7 @@ python3-pip \
 python3-psycopg2 \
 apache2 \
 libapache2-mod-tile \
-#renderd  \
+#renderd  \ # бдем ставить руками, если ставить из пакета. то возникает проблема с работой сервиса renderd
 libmapnik-dev \
 apache2-dev \
 autoconf \
@@ -73,6 +73,10 @@ ttf-unifont
 
 #создадим системного пользователя для работы рендеринга
 sudo add --system --group $USER #имя может быть произвольным, но нне отличимым от того, которого мы создадим позже для БД
+sudo -i
+usermod -aG sudo $USER
+exit
+
 #Добавим АСL и доступ нашему пользователю в нужную директорию
 sudo apt install acl
 sudo setfacl -R -m  u:$USER:rwx /home/$USER/ 
@@ -128,10 +132,88 @@ sudo -u $USER scripts/get-external-data.py
 #устанавливаем шрифты
 scripts/get-fonts.sh
 
+#RENDERD & MOD_TILE
+git clone -b switch2osm https://github.com/SomeoneElseOSM/mod_tile.git
+cd mod_tile
+./autogen.sh
+./configure 
+make
+sudo make install
+sudo make install-mod_tile
+sudo ldconfig
+
 #Создаем директории для РендерД
 mkdir --parents /run/renderd /var/lib/mod_tile/
-sudo chown $USER:$USER /run/renderd/ -R
-sudo chown $USER:$USER /var/lib/mod_tile/ -R
+sudo chown $USER /run/renderd/ -R
+sudo chown $USER /var/lib/mod_tile/ -R
+
+#настраиваем mod_tile
+touch /etc/apache2/conf-available/mod_tile.conf
+cat << EOF > /etc/apache2/conf-available/mod_tile.conf
+LoadModule tile_module /usr/lib/apache2/modules/mod_tile.so
+EOF
+
+#запускаем модуль
+sudo a2enconf mod_tile
+
+#настраиваем renderd
+cat << EOF > /etc/apache2/sites-available/000-default.conf
+<VirtualHost *:80>
+	# The ServerName directive sets the request scheme, hostname and port that
+	# the server uses to identify itself. This is used when creating
+	# redirection URLs. In the context of virtual hosts, the ServerName
+	# specifies what hostname must appear in the request's Host: header to
+	# match this virtual host. For the default virtual host (this file) this
+	# value is not decisive as it is used as a last resort host regardless.
+	# However, you must set it for any further virtual host explicitly.
+	#ServerName www.example.com
+
+	ServerAdmin webmaster@localhost
+        LoadTileConfigFile /etc/renderd.conf
+        ModTileRenderdSocketName /var/run/renderd/renderd.sock
+        # Timeout before giving up for a tile to be rendered
+        ModTileRequestTimeout 0
+	# Timeout before giving up for a tile to be rendered that is otherwise missing
+	ModTileMissingRequestTimeout 30
+        DocumentRoot /var/www/html
+
+	# Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
+	# error, crit, alert, emerg.
+	# It is also possible to configure the loglevel for particular
+	# modules, e.g.
+	#LogLevel info ssl:warn
+
+	ErrorLog ${APACHE_LOG_DIR}/error.log
+	CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+	# For most configuration files from conf-available/, which are
+	# enabled or disabled at a global level, it is possible to
+	# include a line for only one particular virtual host. For example the
+	# following line enables the CGI configuration for this host only
+	# after it has been globally disabled with "a2disconf".
+	#Include conf-available/serve-cgi-bin.conf
+</VirtualHost>
+
+# vim: syntax=apache ts=4 sw=4 sts=4 sr noet
+EOF
+
+#И дважды перезагрузим apache:
+sudo service apache2 reload
+sudo service apache2 reload
+
+# настраиваем рендеринг как юнит 
+#Меняем дефолтного пользователя в конигураци init.d
+sed -i 's/RUNASUSER=renderaccount/RUNASUSER=$USER/g' ~$USER/mod_tile/debian/renderd.init
+#копируем конфигурацию
+sudo cp ~$USER/mod_tile/debian/renderd.init /etc/init.d/renderd
+#меняем права
+sudo chmod 755 /etc/init.d/renderd
+#копируем сервис
+sudo cp ~$USER/mod_tile/debian/renderd.service /lib/systemd/system/
+#запускаем
+sudo /etc/init.d/renderd start
+#Добавляем в автозагрузку
+sudo systemctl enable renderd
 
 #правим конфиг для рендерД
 cat << EOF >> /etc/renderd.conf
@@ -144,29 +226,27 @@ TILESIZE=256
 MAXZOOM=20
 EOF
 
-#Меняем дефолтного пользователя в конигураци init.d
-sed -i 's/RUNASUSER=_renderd/RUNASUSER=$USER/g' /etc/init.d/renderd
 
 #правим сервис рендерД!
-cat << EOF > /usr/lib/systemd/system/renderd.service
-[Unit]
-Description=Daemon that renders map tiles using mapnik
-Documentation=man:renderd
-After=network.target auditd.service
+#cat << EOF > /usr/lib/systemd/system/renderd.service
+#[Unit]
+#Description=Daemon that renders map tiles using mapnik
+#Documentation=man:renderd
+#After=network.target auditd.service
 
-[Service]
-User=$USER
-PIDFile=/var/run/renderd/renderd.pid
-ExecStartPre=/bin/mkdir -p /var/run/renderd
-ExecStart=/usr/bin/renderd -f -c /etc/renderd.conf
-ExecStop=/usr/bin/killall renderd
-Restart=on-failure
-RestartSec=5
+#[Service]
+#User=$USER
+#PIDFile=/var/run/renderd/renderd.pid
+#ExecStartPre=/bin/mkdir -p /var/run/renderd
+#ExecStart=/usr/bin/renderd -f -c /etc/renderd.conf
+#ExecStop=/usr/bin/killall renderd
+#Restart=on-failure
+#RestartSec=5
 
-[Install]
-WantedBy=multi-user.target
-Environment=G_MESSAGES_DEBUG=all
-EOF
+#[Install]
+#WantedBy=multi-user.target
+#Environment=G_MESSAGES_DEBUG=all
+#EOF
 
 #Создаем директорию для юнита и конфига
 mkdir --parents /etc/systemd/system/renderd.service.d/
@@ -204,6 +284,7 @@ wget http://cdn.leafletjs.com/leaflet/v1.7.1/leaflet.zip
 unzip leaflet.zip
 
 #ВАЖНО ТАКЖЕ ЗАРАНЕЕ В СКРИПТЕ УКАЗАТЬ IP СЕРВЕРА в данном случае он отображается и прописывается автоматически,
+
 cat << EOF > index.html
 <!DOCTYPE html>
 <html style="height:100%;margin:0;padding:0;">
